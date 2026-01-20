414aa40350a8c65d4f66b533a0775a59f9b31ea5
italcolapps <italcolapps@gmail.com>
2026-01-20 00:30:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'alnilam-360',
+  webDir: 'www'
+};
+
+export default config;
